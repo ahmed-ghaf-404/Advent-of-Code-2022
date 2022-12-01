52f2283390098515ea586ace60a14b4823327d5a
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2022-12-01 13:33:07 -0500

    added template for solution files

---
+++ temp.py
@@ -0,0 +1,26 @@
+# Date:         December/{}/2022
+# Author:       Ahmed Al Ghafri
+
+def first():
+    # Start Time:   {}
+    # Finish Time:  {}
+    return 
+
+def second():
+    # Start Time:   {}
+    # Finish Time:  {}
+    return 
+
+import datetime
+if __name__=='__main__':
+    # day = str(datetime.datetime.today()).split()[0][-2:]
+    # path = f'Inputs/{day}.txt'
+    # pathTest = f'Inputs/{day}test.txt'
+    
+    # First Quest:
+    print(f"TestCase >> : {first()}")
+    print(f"ActualCase >> : {first()}")
+    # Second Quest:
+    print(f"TestCase >> []: {second()}")
+    print(f"ActualCase >> []: {second()}")
+    
